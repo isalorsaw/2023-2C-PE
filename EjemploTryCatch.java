097baf14c38f87bf19b116f4201a1b8c11c4192a
isalorsaw <isalorsaw@hotmail.com>
2023-05-29 18:23:15 -0600

    Clase 29/5
    2da actualizacion

--- EjemploTryCatch.java
+++ EjemploTryCatch.java
@@ -4,16 +4,21 @@ public class EjemploTryCatch
     public static void main(String args[])
     {
         int num=0;
-        try
-        {
-            //String entrada=javax.swing.JOptionPane.showInputDialog("Ingrese un Numero");
-            String entrada=JOptionPane.showInputDialog("Ingrese un Numero");
-            num=Integer.parseInt(entrada);
-        }
-        catch(Exception exp)
-        {
-            //JOptionPane.showMessageDialog(null,exp);
-            JOptionPane.showMessageDialog(null,"Hubo un error al ingresar numero");
-        }
+        boolean seguir=true;
+        
+        do{
+            try
+            {
+                //String entrada=javax.swing.JOptionPane.showInputDialog("Ingrese un Numero");
+                String entrada=JOptionPane.showInputDialog("Ingrese un Numero");
+                num=Integer.parseInt(entrada);
+                seguir=false;
+            }
+            catch(Exception exp)
+            {
+                //JOptionPane.showMessageDialog(null,exp);
+                JOptionPane.showMessageDialog(null,"Hubo un error al ingresar numero");
+            }
+        }while(seguir==true);   
     }
 }
